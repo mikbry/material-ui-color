ac1d151c26a420545acbb321116f9f3526f5e9bc
Mik <mik.bry@gmail.com>
2020-03-30 12:37:45 +0200

    feat: colorbutton tests (#31)

--- test/ColorButton.test.js
+++ test/ColorButton.test.js
@@ -7,45 +7,63 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import { createMount } from '@material-ui/core/test-utils';
 import renderer from 'react-test-renderer';
+import Tooltip from '@material-ui/core/Tooltip';
 import ColorButton from '../src/components/ColorButton';
-import 'jest-styled-components';
 
-let container = null;
+let mount = null;
 beforeEach(() => {
   // setup a DOM element as a render target
-  container = document.createElement('div');
-  document.body.appendChild(container);
+  mount = createMount();
 });
 
 afterEach(() => {
   // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
+  mount.cleanUp();
+  mount = null;
 });
 
-test('ColorButton no value', () => {
-  const tree = renderer.create(<ColorButton />).toJSON();
+test('ColorButton should renders correctly', () => {
+  const tree = renderer.create(<ColorButton color="darkBlue" />).toJSON();
   expect(tree).toMatchSnapshot();
-  expect(tree).toHaveStyleRule('background-color', 'white');
 });
 
-test('ColorButton color=red', () => {
-  const tree = renderer.create(<ColorButton color="red" />).toJSON();
-  expect(tree).toHaveStyleRule('background-color', 'red');
+test('ColorButton set props', () => {
+  const wrapper = mount(<ColorButton color="" />);
+  let button = wrapper.find(ColorButton);
+  expect(button.props().color).toBe('');
+  expect(button.props().size).toBe(24);
+  expect(button.props().borderWidth).toBe(0);
+  expect(button.props().borderColor).toBe(null);
+  expect(button.props().forwardRef).toBe(null);
+  expect(button.props().tooltip).toBe(null);
+  expect(button).toHaveStyleRule('background-color', 'white');
+  expect(button).toHaveStyleRule('border', '0px solid #767676');
+  wrapper.setProps({ color: 'red', size: 48, borderWidth: 2, borderColor: 'red' });
+  button = wrapper.find(ColorButton);
+  expect(button.props().color).toBe('red');
+  expect(button.props().size).toBe(48);
+  expect(button.props().borderWidth).toBe(2);
+  expect(button.props().borderColor).toBe('red');
+  expect(button).toHaveStyleRule('background-color', 'red');
+  expect(button).toHaveStyleRule('width', '48px');
+  expect(button).toHaveStyleRule('height', '48px');
+  expect(button).toHaveStyleRule('border', '2px solid red');
 });
 
-it('ColorButton onClick', () => {
+test('ColorButton onClick', () => {
   const onClick = jest.fn();
-  act(() => {
-    render(<ColorButton onClick={onClick} />, container);
-  });
-  const button = document.querySelector('.MuiButton-root');
-  act(() => {
-    button.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-  });
+  const wrapper = mount(<ColorButton color="" onClick={onClick} />);
+  const button = wrapper.find(ColorButton);
+  button.simulate('click');
   expect(onClick).toHaveBeenCalledTimes(1);
+});
+
+test('ColorButton toolitip props', () => {
+  const wrapper = mount(<ColorButton color="darkBlue" tooltip="darkBlue" />);
+  const button = wrapper.find(ColorButton);
+  expect(button.props().tooltip).toBe('darkBlue');
+  const tooltip = wrapper.find(Tooltip);
+  expect(tooltip.props().title).toBe('darkBlue');
 });
